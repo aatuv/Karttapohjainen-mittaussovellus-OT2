8b21ea350be580048fff8a443a80d201342f2936
Aatu Väisänen <44797079+aatuv@users.noreply.github.com>
2019-03-26 12:21:11 +0200

    Karttojen käsittely

--- src/components/Kartta.js
+++ src/components/Kartta.js
@@ -1,37 +1,46 @@
 import React, { Component } from 'react';
 import Image from 'react-bootstrap/Image';
-import Spinner from 'react-bootstrap/Spinner';
+import posed from 'react-pose';
 import PropTypes from 'prop-types';
 
 export class Kartta extends Component {
-
-    state = {
-        loading: true
+    constructor(props) {
+        super(props);
+        this.onLoad = this.onLoad.bind(this);
     }
-    /*
-    handleLoading() {
-        new Promise(resolve => {
-                resolve(this.setState({
-                    loading: true
-                }))
-        }).then(() => {
-            setTimeout(() => {
-                this.setState({
-                    loading: false
-                })
-            }, 1000);
+    state = {
+        dimensions: {}
+    }
+    updateLocation(e) {
+        console.log(e.target);
+        let bounds = e.target.getBoundingClientRect();
+        let x = e.clientX - bounds.left;
+        let y = e.clientY - bounds.top;
+        console.log({x, y});
+    }
+        
+    onLoad({target:div}) {
+        this.setState({
+            dimensions: {
+            height: div.offsetHeight,
+            width: div.offsetWidth
+            }
         })
     }
-    */
-   componentDidMount() {
-       this.setState({ loading: false })
-   }
+    
     render() {
+        const Circle = posed.div({
+            draggable: true,
+            pressable: true,
+            init: { scale: 1 },
+            press: { scale: 0.7 },
+            dragBounds: { top: Math.round(-this.state.dimensions.height/2 + 15), left: Math.round(-this.state.dimensions.width/2 + 15), right: Math.round(this.state.dimensions.width/2 - 15), bottom: Math.round(this.state.dimensions.height/2 - 15) }
+        })
         if (this.props.kartta != null) {
             return (
-                this.state.loading ? <Spinner animation="border"></Spinner>
-                : <div className="kartta">
-                    <Image src={this.props.kartta.value} fluid />
+                <div onLoad={this.onLoad} className="kartta">
+                    <Image className="img-kartta" src={this.props.kartta.value} fluid />
+                    <Circle onValueChange={{x: (e) => this.updateLocation(e)}} className="circle" />
                 </div>
             )
         } else {
