c0a4a0e2b7fcf1ab616b12b658b68cf5e9174a50
Siikaes <44649384+Siikaes@users.noreply.github.com>
2019-04-17 10:10:03 +0300

    Add files via upload

--- mittaussovellus/mittaussovellus.py
+++ mittaussovellus/mittaussovellus.py
@@ -6,10 +6,12 @@
 
 import sys
 import iothub_client
-
+import wifi
 import time
 import datetime
 import os
+import urllib
+import getpass
 
 from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
 from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
@@ -17,11 +19,13 @@ from iothub_client import IoTHubClientRetryPolicy, GetRetryPolicyReturnValue
 from iothub_client_args import get_iothub_opt, OptionError
 
 from ruuvitag_sensor.ruuvitag import RuuviTag
-
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+import ruuvitag_sensor.log
 #lampotila, ilmanpaine ja ilmankosteus
 temperature = 0
 pressure = 0
 humidity = 0
+
 
 
 SEND_CALLBACKS = 0
@@ -37,7 +41,7 @@ CONNECTION_STRING = "HostName=OT2.azure-devices.net;DeviceId=raspberry;SharedAcc
 macs = ['F1:BB:17:CC:A9:F1', 'D5:33:97:BA:3A:E6']
 #iothubiin lahetettava tieto
 MSG_TXT = "{\"deviceId\": \"%s \",\"temperature\": %.2f,\"humidity\": %.2f,\"pressure\": %.2f,\"time\": \"%s\"}"
-MSG_TXT_ERROR = "{\"error_deviceId\": \"%s \",\"error_message\": \"%s\",\"error_time\": \"%s\"}"
+#MSG_TXT_ERROR = "{\"error_deviceId\": \"%s \",\"error_message\": \"%s\",\"error_time\": \"%s\"}"
 
 def send_confirmation_callback(message, result, user_context):
     global SEND_CALLBACKS
@@ -48,7 +52,7 @@ def send_confirmation_callback(message, result, user_context):
     key_value_pair = map_properties.get_internals()
     #print ( "    Properties: %s" % key_value_pair )
     SEND_CALLBACKS += 1
-    #print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
+    
 
 
 
@@ -56,6 +60,90 @@ def send_confirmation_callback(message, result, user_context):
 def iothub_client_init():
     client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
     return client
+    
+    
+def test_internet(host='http://google.com'):
+    try:
+        urllib.urlopen(host)
+        return True
+    except:
+        return False
+        
+def Search():
+    wifilist = []
+
+    cells = wifi.Cell.all('wlan0')
+
+    for cell in cells:
+        wifilist.append(cell)
+
+    return wifilist
+        
+def FindFromSearchList(ssid):
+    wifilist = Search()
+
+    for cell in wifilist:
+        if cell.ssid == ssid:
+            return cell
+
+    return False
+        
+def FindFromSavedList(ssid):
+    cell = wifi.Scheme.find('wlan0', ssid)
+
+    if cell:
+        return cell
+
+    return False
+    
+def Add(cell, password=None):
+    if not cell:
+        return False
+
+    scheme = wifi.Scheme.for_cell('wlan0', cell.ssid, cell, password)
+    scheme.save()
+    return scheme   
+    
+def Connect(ssid, password=None):
+    cell = FindFromSearchList(ssid)
+
+    if cell:
+        savedcell = FindFromSavedList(cell.ssid)
+
+        # Already Saved from Setting
+        if savedcell:
+            savedcell.activate()
+            return cell
+
+        # First time to conenct
+        else:
+            if cell.encrypted:
+                if password:
+                    scheme = Add(cell, password)
+
+                    try:
+                        scheme.activate()
+
+                    # Wrong Password
+                    except wifi.exceptions.ConnectionError:
+                        Delete(ssid)
+                        return False
+
+                    return cell
+                else:
+                    return False
+            else:
+                scheme = Add(cell)
+
+                try:
+                    scheme.activate()
+                except wifi.exceptions.ConnectionError:
+                    Delete(ssid)
+                    return False
+
+                return cell
+    
+    return False
 
 def iothub_client_start():
 
@@ -77,7 +165,7 @@ def iothub_client_start():
                         timenow2 = timenow2.strftime("%d.%m.%Y %H:%M:%S")
                         macaddress1 = macs[i]
                         i += 1
-                        message = "hawe"
+                        
                         temperature = 0
                         humidity = 0
                         pressure = 0
@@ -147,6 +235,7 @@ if __name__ == '__main__': #maini paaohjelma
     print ( "\nPython %s" % sys.version )
     print ( "IoT Hub Client for Python" )
     #testataan yhteytta
+    
     try:
         (CONNECTION_STRING, PROTOCOL) = get_iothub_opt(sys.argv[1:], CONNECTION_STRING, PROTOCOL)
     except OptionError as option_error:
@@ -157,5 +246,24 @@ if __name__ == '__main__': #maini paaohjelma
     print ( "Starting the IoT Hub Python Ruuvitag data collector..." )
     print ( "Protocol %s" % PROTOCOL )
     #print ( "    Connection string=%s" % CONNECTION_STRING )
-    
+    #testataan internet yhteytta
+    while (test_internet() == False):
+        print("Ei internet yhteytta")
+        print("valitse SSID")
+        os.system("iwlist wlan0 scan | grep ESSID")
+        ssid = raw_input("Valitse SSID (esim: @wifi.id): ")
+        YN = raw_input("Onko salasanalla suojattu Y/N: ")
+        if (YN == "Y" or YN == "y"):
+            pswd = getpass.getpass('Salasana:')
+            Connect(ssid, pswd)
+        elif(YN == "N" or YN == "n"):
+            Connect(ssid)
+        else:
+            print("Vaara arvo!")
+            continue
+        print ("Sleeping for 10 sec")
+        time.sleep(10)
+    print("Internet yhteys!")
+     
+        
     iothub_client_start()
